50924356217aba62fcac8b119babd0331a3569e9
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-12-11 16:14:02 -0600

    (Corrected after checking against book, I was closer than I thought I would be...)

--- LinkedBinaryTree.py
+++ LinkedBinaryTree.py
@@ -1,8 +1,8 @@
 from BinaryTree import BinaryTree
 
 class LinkedBinaryTree(BinaryTree):
-    class Node:
-        def __init__(self, element, parent, left, right):
+    class _Node:
+        def __init__(self, element, parent=None, left=None, right=None):
             self._element = element
             self._parent = parent
             self._left = left
@@ -25,65 +25,111 @@ class LinkedBinaryTree(BinaryTree):
         self._root = None
         self._size = 0
 
-    def _is_leaf(self, p):
-        node = self._validate(p)
-        return node._left is None and node._right is None
-
     # Private Methods
     def _validate(self, p):
         if isinstance(p, self.Position):
-            raise Exception("Not Valid Position")
+            raise TypeError("p must be proper Position type")
         if p._container is not self:
-            raise Exception("Does Not Belong to this container")
+            raise ValueError("p does not belong to this container")
+        if p._node._parent is p._node:
+            raise ValueError("' is no longer valid")
         return p._node
 
     def _make_position(self, node):
-        return self.Position(self, node)
+        return self.Position(self, node) if node is not None else None
 
     # Public Methods
+    def __len__(self):
+        return self._size
 
-    def add_root(self, e):
-        if self._size != 0:
-            raise Exception("Not Empty")
-        self._root = self.Node(e, None, None, None)
+    def root(self):
         return self._make_position(self._root)
 
-    def add_left(self, p, e):
+    def parent(self, p):
+        node = self._validate(p)
+        return self._make_position(node._parent)
+
+    def left(self, p):
+        node = self._validate(p)
+        return self._make_position(node._left)
+
+    def right(self, p):
+        node = self._validate(p)
+        return self._make_position(node._right)
+
+    def num_children(self, p):
+        node = self._validate(p)
+        count = 0
+        if node._left is not None:
+            count += 1
+        if node._right is not None:
+            count += 1
+        return count
+
+    def _add_root(self, e):
+        if self._root is not None: raise ValueError('Root Exists')
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_position(self._root)
+
+    def _add_left(self, p, e):
         node = self._validate(p)
         if node._left is not None:
-            raise Exception("Already Have A Left Child")
-        newest = self.Node(e, p, None, None)
-        node._left = newest
-        return self._make_position(newest)
+            raise ValueError("Left Child Exists")
+        self._size += 1
+        node._left = self._Node(e, node)
+        return self._make_position(node._left)
 
 
-    def add_right(self, p, e):
+    def _add_right(self, p, e):
         node = self._validate(p)
         if node._right is not None:
-            raise Exception("Already Have A Right Child")
-        newest = self.Node(e, p, None, None)
-        node._right = newest
-        return self._make_position(newest)
+            raise ValueError("Right child exists")
+        self._size += 1
+        node._right = self._Node(e, node)
+        return self._make_position(node._right)
 
-    def replace(self, p, e):
-        prev = self._validate(p)
-        ret = prev._node._element
-        prev._node._element = e
-        return ret
-
-    def delete(self, p):
-        old = self._validate(p)
-        if not self._is_leaf(p)
-            raise Exception("Two children error")
-        old._parent._left = old._left
-        return old._left
-
-    def attach(self, p, T1, T2):
-        if not self._is_leaf(p):
-            raise Exception("Can't combine, p is not a leaf")
+    def _replace(self, p, e):
         node = self._validate(p)
-        node._left, node._right = T1._root, T2._root
-        T1._root, T2._root = None, None
+        old = node._element
+        node._element = e
+        return old
+
+    def _delete(self, p):
+        node = self._validate(p)
+        if self.num_children(p) == 2: raise ValueError('p has two children')
+        child = node._left if node._left else node._right
+        if child is not None:
+            child._parent = node._parent
+        if node is self._root:
+            self._root = child
+        else:
+            parent = node._parent
+            if node is parent._left:
+                parent._left = child
+            else:
+                parent._right = child
+            self._size -= 1
+            node._parent = node
+            return node._element
+
+    def _attach(self, p, t1, t2):
+        node = self._validate(p)
+        if not self.is_leaf(p):raise Exception("position must be leaf")
+        if not type(self) is type(t1) is type(t2):
+            raise TypeError('Tree types must match')
+        self._size += len(t1) + len(t2)
+        if not t1.is_empty():
+            t1._root._parent = node
+            node._left = t1._root
+            t1._root = None
+            t1._size = 0
+        if not t2.is_empty():
+            t2._root._parent = node
+            node._left = t2._root
+            t2._root = None
+            t2._size = 0
+
 
 
 
