84b3e1ef32759bb08d6d06b812eca7859bfcf39b
MadTown86 <grover.donlon@gmail.com>
2023-01-11 21:55:15 -0600

    Further updating as I read.

---
+++ Exercises/R8_5.py
@@ -0,0 +1,12 @@
+"""
+Describe an algorithm - binary tree operations - that counts jst the number of leaves that are left children
+"""
+
+def left_count_start():
+    if t.root().lc:
+        _traverse(t.root().lc)
+
+    pass
+
+
+def _traverse(p):
